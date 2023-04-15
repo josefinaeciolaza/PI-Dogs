4056a9a7e7381b959be95c31ab56d9e87673072d
agustina eciolaza <josefinaecio@gmail.com>
2023-04-15 00:42:34 -0300

    back listo

--- PI-Dogs-main/api/src/routes/index.js
+++ PI-Dogs-main/api/src/routes/index.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
-const { getDogs } = require('../controllers/dogController');
+const { getDogs } = require('../controllers/dogNameController');
+//const { allGetDogs } = require('../controllers/controller');
+const { getRaza } = require('../controllers/dogRazaController');
+const { postDog } = require('../controllers/postController');
+const { getTemperament } = require('../controllers/temperamentController');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,6 +12,10 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/dogs', getDogs);
+router.get('/dogs', getDogs);
+router.get('/dogs/:id', getRaza);
+router.post('/dogs', postDog);
+router.get('/temperament', getTemperament);
+
 
 module.exports = router;
